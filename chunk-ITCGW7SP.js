fb25082d5264312ebfe6e98da0aba500261b8be0
skuetoDev <mariogn1987@gmail.com>
2026-03-01 17:45:47 +0100

    Auto-generated commit

---
+++ chunk-ITCGW7SP.js
@@ -0,0 +1,1 @@
+import{g as a}from"./chunk-YQSFK5VR.js";import{$a as e,Ua as r}from"./chunk-KJL4AFH3.js";var m=class t{constructor(i){this.sanitizer=i}transform(i){return this.sanitizer.bypassSecurityTrustHtml(i)}static \u0275fac=function(n){return new(n||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{m as a};
